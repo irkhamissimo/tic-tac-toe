c274b3fd5787912f337dcea11f1c9bbe38bd509e
irkham <irkham.nurhadi.13@gmail.com>
2024-06-03 14:01:06 +0700

    lifting up state
    lifting up state

---
+++ src/components/Player.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+const Player = ({ initialName, symbol, isActive }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
+
+  function handleClick() {
+    setIsEditing((editing) => !editing);
+  }
+  function handleNameChange(e) {
+    setPlayerName(e.target.value);
+  }
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+  if (isEditing) {
+    editablePlayerName = (
+      <input
+        type="text"
+        value={playerName}
+        required
+        onChange={handleNameChange}
+      ></input>
+    );
+  }
+
+  return (
+    <li className={isActive ? "active" : undefined}>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button type="button" onClick={handleClick}>
+        {isEditing ? "Save" : "Edit"}
+      </button>
+    </li>
+  );
+};
+
+export default Player;
